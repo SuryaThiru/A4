0ac6440b9d433b8bef508d14bfd167b1e48e12ad
vrm-hub <vinay97.pro@gmail.com>
2023-03-12 03:44:28 -0400

    fix: added implementation signature for load

--- src/controller/Image.java
+++ src/controller/Image.java
@@ -1,9 +1,11 @@
 package controller;
 
+import java.io.IOException;
+
 /**
  * This interface acts as a controller in our Image Manipulation Application and delegates tasks
  * respectively.
  */
 public interface Image {
-
+  void load(String imagePath, String imageName) throws IOException;
 }
