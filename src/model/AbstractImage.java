64afbe28c7fcbe2a2b3d98c4871b5bf6eedcad28
vrm-hub <vinay97.pro@gmail.com>
2023-03-13 13:20:41 -0400

    fix: git conflict

--- src/model/AbstractImage.java
+++ src/model/AbstractImage.java
@@ -4,8 +4,6 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import static com.sun.tools.doclint.Entity.image;
-
 public abstract class AbstractImage implements Image {
 
   protected int width;
@@ -22,6 +20,44 @@ public abstract class AbstractImage implements Image {
     this.pixels = new Pixel[height][width];
   }
 
+  @Override
+  public void flipHorizontal() {
+    // Flip color image horizontally
+    Pixel[][] pixels = this.pixels;
+    int width = this.width;
+    int height = this.height;
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width / 2; j++) {
+        Pixel temp = pixels[i][j];
+        pixels[i][j] = pixels[i][width - j - 1];
+        pixels[i][width - j - 1] = temp;
+      }
+    }
+  }
+
+  @Override
+  public void flipVertical() {
+    // Flip color image vertically
+    Pixel[][] pixels = this.pixels;
+    int width = this.width;
+    int height = this.height;
+
+    for (int i = 0; i < height / 2; i++) {
+      for (int j = 0; j < width; j++) {
+        Pixel temp = pixels[i][j];
+        pixels[i][j] = pixels[height - i - 1][j];
+        pixels[height - i - 1][j] = temp;
+      }
+    }
+  }
+
+  /**
+   * This method sets the pixel values at a particular row and column.
+   * @param x represents the row.
+   * @param y represents the column.
+   * @param pixel represents the pixel object to be set at the given place.
+   */
   protected void setPixel(int x, int y, Pixel pixel) {
     pixels[x][y] = pixel;
   }
@@ -36,7 +72,7 @@ public abstract class AbstractImage implements Image {
     return pixels[x][y];
   }
 
-  public void save(String filePath, String fileName) throws IOException {
+  public void save(String filePath) throws IOException {
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
       writer.write(magicNumber);
       writer.write(width + " " + height + "\n");
--- src/model/Image.java
+++ src/model/Image.java
@@ -9,12 +9,26 @@ import java.io.IOException;
 public interface Image {
   void load(String content);
 
-  void save(String filePath, String fileName) throws IOException;
+  void save(String filePath) throws IOException;
 
+  /**
+   * This method flips the image horizontally by swapping each pixel in a row with its
+   * corresponding pixel on the other side of the row.
+   */
   void flipHorizontal();
 
+  /**
+   * This method function flips the image vertically by swapping each pixel in a column with its
+   * corresponding pixel on the other side of the column.
+   */
   void flipVertical();
 
+  /**
+   * This method loops through all pixels in the image, and increases their red, green, and blue
+   * values by the given increment parameter.
+   *
+   * @param increment represents the increment value.
+   */
   void brighten(int increment);
 
   Image[] splitChannels() throws UnsupportedOperationException;
