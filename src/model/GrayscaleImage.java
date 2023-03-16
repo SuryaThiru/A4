1a277e937a0f883d77ccb730590a8f9e7e25938c
vrm-hub <vinay97.pro@gmail.com>
2023-03-15 22:14:06 -0400

    feat: added split method -2

--- src/model/GrayscaleImage.java
+++ src/model/GrayscaleImage.java
@@ -13,26 +13,9 @@ import static helper.ImageUtil.getPixels;
  */
 public class GrayscaleImage extends AbstractImage {
 
-  //TODO we can build a builder class within
-  public GrayscaleImage(int width, int height, int maxValue) {
-    super(width, height, maxValue);
-  }
-
   public GrayscaleImage(int width, int height, int maxValue, Pixel[][] pixels) {
     super(width, height, maxValue);
     this.pixels = pixels;
-  }
-
-  @Override
-  public void load(String content) {
-    Scanner sc = new Scanner(content);
-
-    for (int i = 0; i < height; i++) {
-      for (int j = 0; j < width; j++) {
-        int intensity = sc.nextInt();
-        pixels[i][j] = new Pixel(intensity, intensity, intensity);
-      }
-    }
   }
 
   @Override
@@ -82,42 +65,49 @@ public class GrayscaleImage extends AbstractImage {
 
 
   public Image[] splitChannels() throws UnsupportedOperationException {
-    throw new UnsupportedOperationException("Splitting of greyscale images are not allowed");
+    throw new UnsupportedOperationException("Splitting of one bit images are not allowed");
+  }
+
+  public boolean isGrayscale() {
+    return true;
   }
 
 
   public void combineChannels(Image[] channels) throws IllegalArgumentException {
-    if (channels.length != 3) {
-      throw new IllegalArgumentException("three images are expected to combine the channels");
-    }
-
-    for (int i = 0; i < 3; i++) {
-      channels[i].
-    }
-    if (!((channels[0].width == channels[1].width && channels[0].width == channels[2].width)
-            && (channels[0].height == channels[1].height
-            && channels[0].height == channels[2].height))) {
-      throw new IllegalArgumentException("the three image should be of the same height and width");
-    }
-
-    int width = channels[0].width;
-    int height = channels[0].height;
-
-    RGBImage rgbImage = new RGBImage(width, height, 255);
-    Pixel pixels[][] = new Pixel[width][height];
-
-    for (int y = 0; y < height; y++) {
-      for (int x = 0; x < width; x++) {
-        Pixel pixel = new Pixel();
-        pixel.channels[0] = channels[0].pixels[x][y].getChannels(0);
-        pixel.channels[1] = channels[1].pixels[x][y].getChannels(0);
-        pixel.channels[2] = channels[2].pixels[x][y].getChannels(0);
-
-        pixels[x][y] = pixel;
-      }
-    }
-
-    rgbImage.pixels = pixels;
+//    if (channels.length != 3) {
+//      throw new IllegalArgumentException("three images are expected to combine the channels");
+//    }
+//
+//    for (int i = 0; i < channels; i++) {
+//      if(!channels[i].isGrayscale()) {
+//        throw new IllegalArgumentException("three images are expected to be grayscale images");
+//      }
+//    }
+//
+//    if (!((channels[0].width == channels[1].width && channels[0].width == channels[2].width)
+//            && (channels[0].height == channels[1].height
+//            && channels[0].height == channels[2].height))) {
+//      throw new IllegalArgumentException("the three image should be of the same height and width");
+//    }
+//
+//    int width = channels[0].width;
+//    int height = channels[0].height;
+//
+//    RGBImage rgbImage = new RGBImage(width, height, 255);
+//    Pixel pixels[][] = new Pixel[width][height];
+//
+//    for (int y = 0; y < height; y++) {
+//      for (int x = 0; x < width; x++) {
+//        Pixel pixel = new Pixel();
+//        pixel.channels[0] = channels[0].pixels[x][y].getChannels(0);
+//        pixel.channels[1] = channels[1].pixels[x][y].getChannels(0);
+//        pixel.channels[2] = channels[2].pixels[x][y].getChannels(0);
+//
+//        pixels[x][y] = pixel;
+//      }
+//    }
+//
+//    rgbImage.pixels = pixels;
   }
 }
 
